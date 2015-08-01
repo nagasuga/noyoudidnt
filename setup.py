27ad6294deb548ec3ab3fdbbbf8754dbff8e9177
Jeff Nagasuga <jeffrey.nagasuga@disney.com>
2015-07-31 17:46:12 -0700

    name changed to YouDidn't

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 setup(
-    name='NoYouDidnt',
-    version='0.1.0',
+    name='YouDidnt',
+    version='0.1.1',
     #packages=['noyoudidnt'],
     install_requires=[],
     dependency_links=[],
     description='Analyze left TODOs in a git project',
     author='Jeff Nagasuga',
     author_email='jeffrey.nagasuga@disney.com',
-    url='http://github.com/nagasuga/noyoudidnt',
+    url='http://github.com/nagasuga/youdidnt',
     keywords=[],
     classifiers=[],
-    scripts=['bin/noyoudidnt'],
+    scripts=['bin/youdidnt'],
 )
